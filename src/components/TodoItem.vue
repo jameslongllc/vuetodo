5b6db450f2ec7448f312f8825463931ea113c8b7
James <jameslongllc@gmail.com>
2020-07-31 16:55:00 -0400

    Added Todos Strcuture V-for not working

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="todo-item">
+        <div class="todo-item-content">
+            <div class="todo-item-content-title">{{ title }}</div>
+            <div class="todo-item-content-description">{{ description }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    //     props: ["title", "description"],
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: false,
+            default: "Default description",
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.todo {
+    &-item {
+        background-color: gray;
+        min-height: 70px;
+        margin: 10px;
+        padding: 10px;
+        color: white;
+        border-radius: 5px;
+        font-size: 23px;
+    }
+    &-item-content-title {
+        font-weight: bold;
+    }
+    &-item-content-description {
+        font-size: 19px;
+    }
+}
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,21 +1,73 @@
 <template>
-    <div class="container">
-        <b-card title="Card Title" tag="article" style="max-width: 50rem; " class="mb-2 card" align="center">
-            <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text>
-        </b-card>
+    <div class="todo-wrapper">
+        <div class="todo-container">
+            <!-- <TodoList /> -->
+            <todo-list :todo="todos" />
+        </div>
+        <div class="modal">
+            <div class="modal-content">
+                Modal Window
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import TodoList from "./TodoList";
 export default {
-    // name: 'Home',
+    name: "Todos",
+    components: {
+        TodoList,
+    },
+    data() {
+        return {
+            todos: [
+                {
+                    _id: "1",
+                    title: "Walk The Dog",
+                    description: "Go to forrest near the Zoo",
+                },
+                {
+                    _id: "2",
+                    title: "Buy some bread",
+                    description: "Preferably Potato",
+                },
+                {
+                    _id: "3",
+                    title: "Learn programming",
+                    description: "Now is the time to learn",
+                },
+                {
+                    _id: "4",
+                    title: "Learn Web Development",
+                    description: "Create Web-Base Software",
+                },
+            ],
+        };
+    },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style >
-.card{
-margin: auto;
-}
+<style lang="scss" >
+/* creating a variable in scss */
+$color-red: red;
 
+.todo {
+    &-wrapper {
+        /* flexible styling */
+        display: flex;
+        /* it defines alignment along the main axis */
+        justify-content: center;
+        /*flex-direction: column;*/
+        width: 100%;
+        /*height: 2000px;*/
+    }
+    &-container {
+        width: 400px;
+        min-height: 400px;
+        background-color: #ededed;
+        border-radius: 5px;
+    }
+}
 </style>
